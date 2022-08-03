170f7a39a8f190d9a9f5b15aeb65ccf97c335237
Rusiru Gamage <110513877+IT21819292@users.noreply.github.com>
2022-08-03 04:05:31 +0000

    Exercise 1 - Calculations

--- Tute01.c
+++ Tute01.c
@@ -5,7 +5,19 @@
 #include <stdio.h>
 
 int main() {
+  float sub1,sub2;
+  float avg = 0;
+
+  printf("Please Enter Mark of Subject 01:");
+  scanf("%f",&sub1);
+  printf("Please Enter Mark of Subject 02:");
+  scanf("%f",&sub2);
+
+  avg = (sub1 + sub2)/2;
+
+  printf("Your Average is :%.2f",avg);
   
   return 0;
 }
+
 
